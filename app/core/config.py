4f1745c37297f5565a208f2e9a8a901e17040069
Rohan-Rakesh <rake4046@gmail.com>
2026-03-11 23:01:38 +0530

    Add configuration settings for environment variables

---
+++ app/core/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    MONGO_URI = os.getenv("MONGO_URI")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+settings = Settings()
